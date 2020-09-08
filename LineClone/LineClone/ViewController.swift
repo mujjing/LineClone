a22f2609fe6cba59d053e55f38f7d13c54104510
田 知勲 <j-jihoon@winas.jp>
2020-09-08 22:48:28 +0900

    first commit

---
+++ LineClone/LineClone/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  LineClone
+//
+//  Created by Jh's Macbook Pro on 2020/09/08.
+//  Copyright © 2020 전지훈. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
