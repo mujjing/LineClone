72a23de078cfb118c64ff7468920f8d5a5350193
田 知勲 <j-jihoon@winas.jp>
2020-09-14 01:23:58 +0900

    회원가입 UI작성

---
+++ LineClone/LineClone/ViewController/SignUpViewController.swift
@@ -0,0 +1,18 @@
+//
+//  SignUpViewController.swift
+//  LineClone
+//
+//  Created by Jh's Macbook Pro on 2020/09/14.
+//  Copyright © 2020 전지훈. All rights reserved.
+//
+
+import UIKit
+
+class SignUpViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+}
